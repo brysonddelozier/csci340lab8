7e81a0f91a6a7a931398d168697336e737d0fef6
brysonddelozier <delozierbryson@gmail.com>
2026-03-12 18:53:58 -0500

    end of step 8

--- RazorPagesMeme/Models/Meme.cs
+++ RazorPagesMeme/Models/Meme.cs
@@ -6,10 +6,19 @@ namespace RazorPagesMeme.Models;
 public class Meme
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Name { get; set; } = string.Empty;
+
     [DataType(DataType.Date)]
+    [Required]
     public DateTime Date { get; set; }
+
     public string? Origin { get; set; }
+    
+    [Range(1, 10)]
+    [DataType(DataType.Integer)]
     public int Funniness { get; set; }
 
     [Display(Name = "Celebrity Endorsement")]
